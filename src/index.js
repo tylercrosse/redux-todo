1c16ab6301614db7e6975a357b25221ebfb45ecd
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-10-31 19:01:39 -0700

    Todo app with reducers and react view

--- src/index.js
+++ src/index.js
@@ -1,47 +1,105 @@
-import React from 'react';
+import React    from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
 // import App from './App';
 // import './index.css';
 
-import { createStore } from 'redux';
-
-function counter(state = 0, action) {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
+// individual todos
+function todo(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      } else {
+        return {
+          ...state,
+          completed: !state.completed
+        };
+      }
     default:
       return state;
   }
 }
 
-const store = createStore(counter);
+// todos array
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+};
 
-function Counter({ value, onIncrement, onDecrement }) {
-  return (
-    <div>
-      <h1>{value}</h1>
-      <button onClick={onIncrement}>+</button>
-      <button onClick={onDecrement}>-</button>
-    </div>
-  )
+function visibilityFilter(state = 'SHOW_ALL', action) {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
+
+const store = createStore(todoApp);
+
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node;
+        }}/>
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          this.input.value = '';
+        }}>Add todo</button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                });
+              }}
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none'
+              }}
+            >
+              {todo.text}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
 
 function render() {
   ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => 
-        store.dispatch({
-          type: 'INCREMENT'
-        })
-      }
-      onDecrement={() => 
-        store.dispatch({
-          type: 'DECREMENT'
-        })
-      }
+    <TodoApp
+      todos={store.getState().todos}
     />,
     document.getElementById('root')
   );
