b5f969f24d484eb48ace77c3d22959dd84aaf7cb
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-10-31 17:01:17 -0700

    Simple counter example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { createStore } from 'redux';
+
+function counter(state = 0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+function Counter({ value, onIncrement, onDecrement }) {
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  )
+}
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => 
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() => 
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+    />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
